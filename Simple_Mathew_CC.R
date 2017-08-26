bbc677b2f1f92fdc4799b11a1b2f3d274f1f0caf
Roberto Ruiz <robertoruizicochea@gmail.com>
2017-08-26 22:59:50 +0200

    Simple mcc
    Simple Mathew coefficient for comparison between two binary vectors.

---
+++ Simple_Mathew_CC.R
@@ -0,0 +1,18 @@
+
+
+mcc <- function (actual, predicted)
+{
+  TP <- sum(actual == 1 & predicted == 1)
+  TN <- sum(actual == 0 & predicted == 0)
+  FP <- sum(actual == 0 & predicted == 1)
+  FN <- sum(actual == 1 & predicted == 0)
+ 
+  sum1 <- TP+FP; sum2 <-TP+FN ; sum3 <-TN+FP ; sum4 <- TN+FN;
+  denom <- as.double(sum1)*sum2*sum3*sum4 
+  if (any(sum1==0, sum2==0, sum3==0, sum4==0)) {
+    denom <- 1
+  }
+  mcc <- ((TP*TN)-(FP*FN)) / sqrt(denom)
+  return(mcc)
+}
+
